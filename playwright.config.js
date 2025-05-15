e40e33b5b5d4d6e7d2f30405fc670dfd2adb6deb
pkumv1 <45692629+pkumv1@users.noreply.github.com>
2025-05-15 19:02:03 +0530

    Added Playwright config

---
+++ playwright.config.js
@@ -0,0 +1,45 @@
+// @ts-check
+const { defineConfig, devices } = require('@playwright/test');
+
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ */
+module.exports = defineConfig({
+  testDir: './test_scripts/playwright',
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 1,
+  workers: process.env.CI ? 1 : 3,
+  reporter: [
+    ['html', { outputFolder: './results/playwright-report' }],
+    ['json', { outputFile: './results/playwright-results.json' }]
+  ],
+  use: {
+    baseURL: 'https://the-internet.herokuapp.com',
+    trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+    video: 'on-first-retry',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
+    {
+      name: 'mobile-chrome',
+      use: { ...devices['Pixel 5'] },
+    },
+    {
+      name: 'mobile-safari',
+      use: { ...devices['iPhone 12'] },
+    },
+  ],
+});
